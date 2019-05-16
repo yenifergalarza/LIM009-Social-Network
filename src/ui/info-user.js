2f6ec9c5744925d6ca03253355bfe334600a00a1
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-05-15 23:02:20 -0500

    estilos de desktop orden bien falta wallpaper

--- src/ui/info-user.js
+++ src/ui/info-user.js
@@ -1,84 +1,93 @@
 import { signOutUser } from '../lib/view-controllers/auth.js';
-import { currentUser } from '../lib/controller-firebase/auth.js';
-import { getRealTimePost, getUserData, getRealTimeData  } from '../lib/controller-firebase/firestore.js';
-import { addNewPost,getUser} from '../lib/view-controllers/firestore.js';
-export const Content = () => {
+import { addNewPost, getUser, deletePosts, editPosts } from '../lib/view-controllers/firestore.js';
+
+const listPosts = (publi) => {
+  console.log(publi)
+  const div = document.createElement('div')
+  const publicacion = `
+    <div class="comment-post post">
+      <div class="owner-post">
+        <h3 class="font-size-post white ">Publicado por <span>${publi.doc.user}</span> </h3>
+        <div class="cross" id="delete"></div>
+      </div>
+      <div class="font-size-post text-post"> ${publi.doc.post}</div>
+      <input id="update-data" value=${publi.doc.post} />
+      <button id="edit"> Editar </button>
+       <div class="color-post">
+        <div class="container-click reaction">
+          <div type="button" id="like" class="button-like click button-icon"> ${publi.doc.likes} </div>
+          <div id="edit" class=" button-paperPlane click button-icon"></div>
+        </div>
+      </div>
+    </div>
+  `
+  div.innerHTML = publicacion;
+  const btnDelete = div.querySelector('#delete');
+  btnDelete.addEventListener('click', () => deletePosts(publi))
+
+  const btnEdit = div.querySelector('#edit');
+  const updateData = div.querySelector('#update-data')
+  btnEdit.addEventListener('click', () => editPosts(publi, updateData.value))
+
+  return div
+}
+
+export const Content = (posts) => { 
   const div = document.createElement('div');
   div.innerHTML = `
-<nav class="nav"> 
-<div class="nav-content">
-<div id="btn-out" class="menu"></div>
-<h1 class="white">Welcome </h1>
-</div>
-</nav>
-<div class="container" >
-<div class="container-user" id="printinfo">
-   
-   
-</div>
+  <nav class="nav">
+    <div class="nav-content">
+      <div id="show-menu" class="menu"></div>
+      <h1 class="white">Welcome </h1>  <button id="btn-out">Cerrar sesión</button>
+    </div>
+  </nav>
 
-<div class="posts"  id="allContainer">
-<div id="editpost" class="edit-post post ">
-<textarea name="" id="write-post" cols="30" rows="10" class="write-post "></textarea>
-  <div class="container-click">
-  <div type="button" id="add-image" class="button-photo click button-icon"> </div>
-  <div id="add" class="button-share click white">comparte</div>
-  
+  <div class="container" id="print-info">
+
+
+
+    <div class="posts">
+    <div class="edit-post post ">
+      <textarea name="" id="comment" cols="30" rows="10" class="write-post "></textarea>
+      <div class="container-click">
+        <div type="button" id="add-image" class="button-photo click button-icon"> </div>
+        <button id="add" class="button-share click white"> Publicar </button>
+      </div>
+    </div>
+    <div id="post-added"></div>
   </div>
-</div>
+  </div>
 
-</div> 
-</div>
   `;
+  
   const buttonLogOut = div.querySelector('#btn-out');
-  const printinfo = div.querySelector('#printinfo');
-  const comment = div.querySelector('#comment');
+  const printinfo = div.querySelector('#print-info');
+  const comment = div.querySelector('#comment'); 
   const add = div.querySelector('#add');
-  const postAdded = div.querySelector('#postAdded');
-  const allContainer = div.querySelector('#allContainer');
+  const postAdded = div.querySelector('#post-added');
 
   buttonLogOut.addEventListener('click', signOutUser)
   getUser((myData) => {
-    printinfo.innerHTML = `
-   
-    <img class="user-image-landscape" src=""../src/assets/picture.png">
-    <div class="user-photo-name"><img class="user-photo" src="${myData.photo}" alt="">
-    <div class="text-user">
-    <p></p>
-    <p>${myData.name}</p></div>
-    </div>
-  `
-
-
+    printinfo.innerHTML += `
+  <div class="container-user">
+  <div class="user-image-landscape">
+  </div>
+  
+  <div class="user-photo-name">
+  <img class="user-photo" src="${myData.photo}" alt="">
+  <div class="text-user">
+  <p> ${myData.name}</p>
+  <p>developer jr</p></div>
+          `
   });
 
-  const user = currentUser()
   add.addEventListener('click', () => {
-    addNewPost(user)
+    addNewPost(comment)
   });
-  getRealTimePost((posts) => {
-    posts.forEach(publi => {
-      console.log(publi)
-      const div = document.createElement('div')
-      const publicacion = `
-      <div class="comment-post post">
-      <div class="owner-post">
-      <h3 class="font-size-post white ">Publicado por ${publi.user}</h3>
-      <div class="cross" id="delete"></div>
-      </div>
-      <p class="font-size-post text-post">${publi.post}</p>
-      <div class="color-post">
-      <div class="container-click reaction">
-      <div type="button" id="like" class="button-like click button-icon"> </div>
-      <div id="edit" class=" button-paperPlane click button-icon"></div>
-      
-      </div>
-      
-      
-       `
-      allContainer.innerHTML += publicacion
-      return div
-    })
-  });
+
+  posts.forEach(publi => {
+    postAdded.appendChild(listPosts(publi))
+  })
+  
   return div
 };
