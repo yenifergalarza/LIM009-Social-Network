8050c71eda7b726afc0c2ea5318e03ef258bdb01
kathmontalvo <kathmontalvo123@gmail.com>
2019-05-18 01:30:35 -0500

    changes merged test

--- test/post.spec.js
+++ test/post.spec.js
@@ -1,32 +1,40 @@
+import MockFirebase from 'mock-cloud-firestore';
+import { getRealTimeData, getRealTimePost, addPost, deletePost, editPost, likePlus, updateUser } from "../src/lib/controller-firebase/firestore.js";
 
-  import MockFirebase from 'mock-cloud-firestore';
-  const fixtureData = {
-    __collection__: {
-      posts: {
-        __doc__: {
-            123456: {     
-likes: 0,post : "hola",privacy :"private",state : true,uid :"mZlFTubNrZPWBPQeMxUVoXX0exy1" ,user :"juanita"
-} 
-          },
-          234567: {     
-        likes: 0, post :"adios",privacy :"public",state : true,uid :"mZlFTubNrZPWBPQeMxUVoXX0exy1" ,user :"perlita" 
-                      },
+const fixtureData = {
+  __collection__: {
+    posts: {
+      __doc__: {
+        123456: {
+          likes: 0, post: "hola", privacy: "private", state: true, uid: "mZlFTubNrZPWBPQeMxUVoXX0exy1", user: "juanita"
         }
-      }
+      },
+      234567: {
+        likes: 0, post: "adios", privacy: "public", state: true, uid: "mZlFTubNrZPWBPQeMxUVoXX0exy1", user: "perlita"
+      },
     }
-    const user = {displayName:"juanita"};
-  
-  global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true });
-  
-  import { getRealTimeData,getRealTimePost, addPost, deletePost, editPost ,likePlus,updateUser} from "../src/lib/controller-firebase/firestore.js";
-describe('addPost',()=>{
-    it('deberia agreagar un post ',(done)=>{
-        return addPost('hola',user,0,"private").then((data)=>{
-            const callback =(notes)=>{
-                console.log(notes);
-            done()
-            }
-            getRealTimePost(callback)
-                })
+  }
+}
+global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true });
+
+describe('agregar posts', () => {
+  it('deberia agregar un post', (done) => {
+    return addPost('quiero leer', 'Kath Montalvo', 'abc123', 1, 'public')
+      .then((res) => {
+        expect(res._data.post).toBe('quiero leer')
+        done()
+      })
+  })
+})
+
+describe('addPost', () => {
+  it('deberia agreagar un post ', (done) => {
+    return addPost('quiero leer', 'Kath Montalvo', 'abc123', 1, 'public').then(() => {
+      const callback = (notes) => {
+        console.log(notes);
+        done()
+      }
+      getRealTimePost(callback)
     })
+  })
 })
