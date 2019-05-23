c7662c9b13a667394af0ac5156cdc0953960b89d
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-05-22 20:54:00 -0500

    test

--- test/posts-actions.spec.js
+++ test/posts-actions.spec.js
@@ -1,57 +1,48 @@
 
-import { privacyPost, likePlus,addComment,deleteComment} from "../src/lib/controller-firebase/posts-actions";
-import { getRealTimePost} from "../src/lib/controller-firebase/posts.js";
-
 import MockFirebase from 'mock-cloud-firestore';
+
 const fixtureData = {
-    __collection__: {
-        users: {
-            __doc__: {
-                axxYZ12: {
-                    name: 'Pepi',
-                    email: 'pepi@mail.com',
-                    photo: 'pepita.jpg',
-                    uid: 'axxYZ12'
-                }
-            }
-        },
-        posts: {
-            __doc__: {
-                123456: {
-                    likes: 0,
-                    post: "hola",
-                    privacy: "private",
-                    state: true,
-                    uid: "mZlFTubNrZPWBPQeMxUVoXX0exy1",
-                    user: "Yeni",
-                    __collection__: {
-          
-                    comments:{
-                      __doc__: {
-                        1234567: {
-                          date:{seconds: 1558310759, nanoseconds: 213000000},
-                        post:"holaaaa",
-                      uid: "qwerty",
-                      user:"Pepito suarez",
-                      },
-                    },
-              
-                  }
-              },
-            },
-            234567: {
-                likes: 0,
-                post: "adios",
-                privacy: "public",
-                state: true,
-                uid: "mZlFTubNrZPWBPQeMxUVoXX0exy1",
-                user: "perlita"
-            },
-        },
+  __collection__: {
+    users: {
+      __doc__: {
+        axxYZ12: {
+          name: 'Pepi',
+          email: 'pepi@mail.com',
+          photo: 'pepita.jpg',
+          uid: 'axxYZ12'
+        }
+      }
     },
+    posts: {
+      __doc__: {
+        123456: {
+          likes: 0,
+          post: "hola",
+          photo: "yeni.jpg",
+          privacy: "private",
+          uid: "mZlFTubNrZPWBPQeMxUVoXX0exy1",
+          user: "Yeni",
+          date: { seconds: 1558322863, nanoseconds: 816000000 }
+        }
+      },
+      234567: {
+        likes: 0,
+        post: "adios",
+        photo: "perlita.jpg",
+        privacy: "public",
+        uid: "mZlFTubNrZPWBPQeMxUVoXX0exy1",
+        user: "perlita",
+        date: { seconds: 1558310759, nanoseconds: 213000000 }
+      },
+    }
+  }
 }
 
 global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true });
+
+
+import { privacyPost, likePlus,addComment,deleteComment} from "../src/lib/controller-firebase/posts-actions.js";
+import { getRealTimePost} from "../src/lib/controller-firebase/posts.js";
 
 
 describe('editar post', () => {
@@ -81,32 +72,33 @@ describe('editar post', () => {
 
 
 describe('agregar comentario', () => {
-    it('deberia agregar un comentario ', (done) => {
-      return addComment(
-        'Dame croquetas','Pepi',axxYZ12,123456).then(() => {
-        const callback = (notes) => {
-          const result = notes.filter((note) => {
-            return note.doc.post === 'Dame croquetas';
-          })
-          expect(result[0].doc.post).toBe('Dame croquetas');
-          done()
-        }
-        getRealTimePost(callback)
+    it.only('deberia agregar un comentario ', (done) => {
+      return addComment('Dame croquetas','Pepi','axxYZ12',234567).then((a) => {
+          console.log(a)
+        // const callback = (notes) => {
+        //   const result = notes.filter((note) => {
+        //     return note.doc.post === 'Dame croquetas';
+        //   })
+        //   expect(result[0].doc.post).toBe('Dame croquetas');
+        //   done()
+        // }
+        // getRealTimeComment(123456,callback)
       })
     })
   })
   
   describe('eliminar comentario', () => {
-    it('debe eliminar post con id 234567', (done) => {
-      return deleteComment('234567').then(() => {
+    const newLocal = 1234567;
+    it('debe eliminar post con id 1234567', (done) => {
+      return deleteComment(newLocal).then(() => {
         const callback = (notes) => {
           const result = notes.filter((note) => {
-            return note.id === '234567';
+            return note.id === 1234567;
           }) 
           expect(result[0]).toBe(undefined)
          done()
         }
-        getRealTimePost(callback)
+        getRealTimeComment(123456,callback)
       })
     })
   })
